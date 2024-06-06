1ab22b9d87674eb47825b48fed224f7ef29a1d81
Maksim <45427901+flydecisive@users.noreply.github.com>
2024-06-06 22:52:24 +0300

    refactor add button

--- src/components/UI/buttons/add/AddButton.tsx
+++ src/components/UI/buttons/add/AddButton.tsx
@@ -1,10 +1,43 @@
+"use client";
+
+import { useRef, useState } from "react";
 import styles from "./AddButton.module.scss";
+import PlusIcon from "@/components/icons/plus/PlusIcon";
+
 import React from "react";
 
 interface AddButtonProps {
-  children: Readonly<React.ReactNode>;
+  children: React.ReactNode;
 }
 
 export default function AddButton({ children }: AddButtonProps) {
-  return <div className={styles.button}>{children}</div>;
+  const [isHover, setIsHover] = useState(false);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleButtonClick = () => {
+    if (inputRef.current) {
+      inputRef.current.click();
+    }
+  };
+
+  return (
+    <>
+      <input type="file" style={{ display: "none" }} ref={inputRef} />
+      <div
+        className={styles.button}
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+        onClick={handleButtonClick}
+      >
+        <PlusIcon isHover={isHover} />
+        <p
+          className={
+            isHover ? `${styles.text} ${styles.text_hovering}` : styles.text
+          }
+        >
+          {children}
+        </p>
+      </div>
+    </>
+  );
 }
--- src/pages/wallets/right/WalletsRight.tsx
+++ src/pages/wallets/right/WalletsRight.tsx
@@ -1,13 +1,13 @@
 import styles from "./WalletsRight.module.scss";
 import Wallet from "@/components/wallet/Wallet";
-import AddWalletButton from "@/components/UI/buttons/add-wallet/AddWallet";
+import AddButton from "@/components/UI/buttons/add/AddButton";
 
 export default function WalletsRight() {
   return (
     <div className={styles.container}>
       <div className={styles.wallets}>
         <Wallet />
-        <AddWalletButton />
+        <AddButton>Добавить кошелек</AddButton>
       </div>
     </div>
   );
